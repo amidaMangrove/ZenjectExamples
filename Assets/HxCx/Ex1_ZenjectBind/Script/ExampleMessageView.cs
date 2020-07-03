296d72756a33acc8fb3d617c5d6d27ce68e0b270
s-sawaguchi <sawaguchi0604@gmail.com>
2020-07-03 22:45:48 +0900

    add : Zenject Binding Sample

---
+++ Assets/HxCx/Ex1_ZenjectBind/Script/ExampleMessageView.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace HxCx {
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ExampleMessageView : MonoBehaviour {
+
+        Text _message;
+        Text Message
+        {
+            get { return _message ?? GetComponent<Text>(); }
+        }
+
+        /// <summary>
+        /// メッセージを更新する
+        /// </summary>
+        /// <param name="message"></param>
+        public void SetMessage(string message)
+        {
+            Message.text = message;
+        }
+    }
+}
---
+++ Assets/HxCx/Ex1_ZenjectBind/Script/Presenter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Zenject;
+
+namespace HxCx {
+    public class Presenter : MonoBehaviour {
+
+        Button _changeButton;
+        ExampleMessageView _exampleView;
+
+        [Inject]
+        public void Constructor(Button button, ExampleMessageView exampleView)
+        {
+            _changeButton = button;
+            _exampleView = exampleView;
+        }
+        // Start is called before the first frame update
+        void Start()
+        {
+            _changeButton.onClick.AddListener(() =>
+            {
+                _exampleView.SetMessage("Fuga!");
+            });
+        }
+    }
+}
